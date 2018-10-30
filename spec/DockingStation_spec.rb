93a3f91fde16c60a29928be77472f8d8954271a7
Obsiye <mrabdirizak@gmail.com>
2018-10-30 17:22:58 +0000

    Add unit tests and Refactor code
    - Create unit test in rspec for getting a bike and expecting it to be working
    - Refactor the code to pass the above tests

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,6 +1,13 @@
 require 'docking_station'
 
 describe DockingStation do 
-  it { expect(DockingStation.new).to respond_to(:release_bike) }
-  it { expect(:release_bike).to eq Bike.new }
+docking_station = DockingStation.new
+
+  it { expect(docking_station).to respond_to(:release_bike) }
+  it { expect(docking_station.release_bike).to be_instance_of(Bike) }
+
+  it "gets a bike and expect it to work" do
+    expect(docking_station.release_bike.working?).to eq true
+  end
+
 end
